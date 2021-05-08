3e5915a5ad907f6328eb72652cb2f237cca51d8c
david26694 <david26694@gmail.com>
2021-05-08 23:41:54 +0200

    Add more misspellings

--- 03_sklearn_baseline.py
+++ 03_sklearn_baseline.py
@@ -7,6 +7,7 @@ from stop_words import get_stop_words
 from tqdm import tqdm
 from sklearn.model_selection import train_test_split
 from sklearn.linear_model import LogisticRegression
+from lightgbm import LGBMClassifier
 from sklearn.pipeline import Pipeline, FeatureUnion
 from sklego.preprocessing import ColumnSelector, ColumnDropper
 from sklearn.metrics import f1_score
@@ -17,11 +18,16 @@ stop_words = get_stop_words('catalan')
 train = pd.read_csv("data/train.csv").rename(columns={"TÍTOL": "title", "Codi QdC": "code"})
 test = pd.read_csv("data/test.csv").rename(columns={"TÍTOL": "title", "Codi QdC": "code"})
 categories = pd.read_csv("data/categories.csv").drop(columns="Unnamed: 0").rename(columns={"Codi QdC": "code", "Títol de entrada del catàleg": "target_title"})
+
 # %%
-print(stopwords.words('spanish'))
-categories
-# %%
-stop_words = stop_words + ['d', 'l', 'al', 'del', 'a', 'dels', 'als']
+# IDEAS:
+# MAJUSCULES
+# STEMMING
+# SANT JORDI
+# SINGULARS
+# 
+
+stop_words = stop_words + ['d', 'l', 'al', 'del', 'a', 'dels', 'als', 'deun', 'deuna']
 
 misspellings = {
     "anul la": "anulla",
@@ -30,7 +36,13 @@ misspellings = {
     "al legac": "allegac",
     "parcel lac": "parcellac",
     "col labora": "collabora",
-    "sol lici": "sollici"
+    "sol lici": "sollici",
+    " dea": " de a",
+    "sant jor ": "sant jordi ",
+    "devuit": "18",
+    "denou": "19",
+    "resi dencia": "residencia",
+    "deta ": "data "
 }
 # %%
 stop_words
@@ -55,7 +67,8 @@ categories["target_title"] = preprocess_str(categories["target_title"], stop_wor
 train["title"] = preprocess_str(train["title"], stop_words, misspellings)
 test["title"] = preprocess_str(test["title"], stop_words, misspellings)
 # %%
-train.to_csv("data/train_cleanead.csv", index=False)
+# train.merge(categories, on="code", how='left').to_csv("data/train_cleanead.csv", index=False)
+train.merge(categories, on="code", how='left').sort_values("target_title").to_csv("data/train_cleanead.csv", index=False)
 
 # %%
 def n_match_target(x, set_target):
@@ -63,7 +76,9 @@ def n_match_target(x, set_target):
     return len(set_x & set_target) / len(set_target)
 
 # %%
-for i, target in tqdm(enumerate(categories.target_title)):
+train_cats = train.merge(categories, on='code').target_title.unique()
+# %%
+for i, target in tqdm(enumerate(train_cats)):
     set_target = set(target.split())
     # n_match_target("lolo", set_target)
     train[f"title_{i}_match"] = train.title.apply(lambda x: n_match_target(x, set_target))
@@ -122,10 +137,15 @@ lr = LogisticRegression()
 lr.fit(train_X_feats, train_y)
 
 # %%
-f1_score(lr.predict(train_X_feats), train_y, average='micro')
+print(f1_score(lr.predict(val_X_feats), val_y, average='micro'))
+print(f1_score(lr.predict(train_X_feats), train_y, average='micro'))
 
 # %%
-f1_score(lr.predict(val_X_feats), val_y, average='micro')
+# lgb = LGBMClassifier()
+# lgb.fit(train_X_feats, train_y)
+# print(f1_score(lgb.predict(val_X_feats), val_y, average='micro'))
+# print(f1_score(lgb.predict(train_X_feats), train_y, average='micro'))
+
 # %%
 test_predictions = lr.predict(test_X_feats)
 # %%
@@ -135,6 +155,6 @@ submission = test.copy().loc[:, ["ID"]]
 # %%
 submission["Codi QdC"] = test_predictions
 # %%
-submission.to_csv("submissions/model_with_text_feats.csv", index=False)
+submission.to_csv("submissions/04_better_misspellings.csv", index=False)
 
 # %%
